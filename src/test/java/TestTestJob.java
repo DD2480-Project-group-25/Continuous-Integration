7f46e7f8ee31454023d185b1f0c4109a5c382ad9
Helena Alinder <helenaalinder@gmail.com>
2019-02-04 15:57:10 +0100

    test unvalid name of cloned repository

--- src/test/java/TestTestJob.java
+++ src/test/java/TestTestJob.java
@@ -1,14 +1,13 @@
-import org.junit.Assert;
 import org.junit.Test;
 import se.kth.dd2480.grp25.ci.*;
 
 public class TestTestJob {
 
-  @Test
-  public void testTestJobRun() {
-    Event event = new Event();
+  /** There is no cloned repository with the name 123, so exception is thrown. */
+  @Test(expected = IllegalStateException.class)
+  public void testTestJobException() {
+    Event event = new Event("123", Event.EventType.TEST);
     TestJob job = new TestJob(event);
     job.run();
-    Assert.assertEquals("All tests passed.", event.getMessage());
   }
 }
