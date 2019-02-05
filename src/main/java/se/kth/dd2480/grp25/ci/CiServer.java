11937f397c18a913c7d884b6b3eba71d92268cd0
Marcus Granström <marcus.granstroem@gmail.com>
2019-02-05 16:27:37 +0100

    adds a first version of webhook handling
    This commit is also a test to see if the webhook is triggered

--- src/main/java/se/kth/dd2480/grp25/ci/CiServer.java
+++ src/main/java/se/kth/dd2480/grp25/ci/CiServer.java
@@ -19,6 +19,7 @@ public class CiServer {
   public CiServer() throws IOException {
     server = HttpServer.create(new InetSocketAddress(port), 0);
     server.createContext("/").setHandler(CiServer::handleHttpRequest);
+    server.createContext("/hooks/github").setHandler(CiServer::handleWebhook);
     server.start();
     System.out.println("Server running...");
   }
@@ -29,6 +30,30 @@ public class CiServer {
     OutputStream os = exchange.getResponseBody();
     os.write(response.getBytes());
     os.close();
+  }
+
+  private static void handleWebhook(HttpExchange exchange) throws IOException {
+    if (exchange.getRequestMethod().equalsIgnoreCase("POST")) {
+      // Should perhaps write to logfile
+      System.out.println("POST REQUEST");
+      exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0);
+    } else {
+      System.out.println("INVALID REQUEST");
+      exchange.sendResponseHeaders(HttpURLConnection.HTTP_BAD_REQUEST, 0);
+      exchange.close();
+      return;
+    }
+
+    int i;
+    String req_body = "";
+    InputStream is = exchange.getRequestBody();
+    // Read all content of input stream
+    while ((i = is.read()) != -1) {
+      req_body += (char) i;
+    }
+    System.out.println(req_body);
+    is.close();
+    exchange.close();
   }
 
   public void stop() {
