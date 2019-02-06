87c846d8bf4d19ba3ecfe34f2030d572d9c5ed60
Marcus Granström <marcus.granstroem@gmail.com>
2019-02-06 02:10:51 +0100

    Added webhook listener to CiServer
    This handler listens on /hooks/github for incoming push events
    
    One helper method is inluded to parse the json object

--- src/main/java/se/kth/dd2480/grp25/ci/CiServer.java
+++ src/main/java/se/kth/dd2480/grp25/ci/CiServer.java
@@ -1,8 +1,15 @@
 package se.kth.dd2480.grp25.ci;
 
-import com.sun.net.httpserver.*;
-import java.io.*;
-import java.net.*;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
 
 /**
  * A simple HttpServer that at the moment respond with "Hello World!" to localhost on port 8000, no
@@ -34,11 +41,8 @@ public class CiServer {
 
   private static void handleWebhook(HttpExchange exchange) throws IOException {
     if (exchange.getRequestMethod().equalsIgnoreCase("POST")) {
-      // Should perhaps write to logfile
-      System.out.println("POST REQUEST");
       exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0);
     } else {
-      System.out.println("INVALID REQUEST");
       exchange.sendResponseHeaders(HttpURLConnection.HTTP_BAD_REQUEST, 0);
       exchange.close();
       return;
@@ -51,9 +55,36 @@ public class CiServer {
     while ((i = is.read()) != -1) {
       req_body += (char) i;
     }
-    System.out.println(req_body);
+
     is.close();
     exchange.close();
+
+    // Parse payload form POST request
+    String commitID = parseJsonString(req_body, "after");
+
+    if (commitID == "INVALID") {
+      // Should perhaps be logged
+      System.out.println("Invalid JSON file provided");
+      return;
+    }
+    System.out.println(commitID);
+  }
+
+  public static String parseJsonString(String json, String arg) {
+    String res = "";
+    JsonParser parser;
+    parser = new JsonParser();
+    try {
+      JsonElement jsonElem = parser.parse(json);
+      res = jsonElem.getAsJsonObject().get(arg).getAsString();
+    } catch (JsonSyntaxException e) {
+      // Should perhaps be written to logfile
+      res = "INVALID";
+    }
+
+    // Remove double quotes from parsing
+    res = res.replaceAll("\"", "");
+    return res;
   }
 
   public void stop() {
