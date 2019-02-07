3b6a097bb74993859ff49d32ee3c0b3c4b8a0b50
Helena Alinder <helenaalinder@gmail.com>
2019-02-07 21:36:43 +0100

    remove test that can only be run locally

--- src/test/java/TestCloneJob.java
+++ src/test/java/TestCloneJob.java
@@ -4,31 +4,6 @@ import org.junit.Test;
 import se.kth.dd2480.grp25.ci.*;
 
 public class TestCloneJob {
-  @Test
-  public void testCloneJobTestPass() {
-    EventQueue queue = new EventQueue();
-    Event event =
-        new Event(
-            "dbbf1908486644822e0189be7b88678c3aec7313",
-            Event.Type.CLONE,
-            Event.Status.SUCCESSFUL,
-            "Nothing",
-            "DD2480-Project-group-25/Continuous-Integration");
-    CloneJob job = new CloneJob(event, queue);
-    job.run();
-    try {
-      Event cloneEvent = queue.pop();
-      Assert.assertEquals(Event.Status.SUCCESSFUL, cloneEvent.getStatus());
-      Assert.assertEquals("The repository is cloned successfully", cloneEvent.getMessage());
-
-      String command = "rm -rf " + event.getId();
-      Runtime.getRuntime().exec(command, null, new File(".")).waitFor();
-
-    } catch (Exception e) {
-      System.out.println(e);
-      Assert.fail();
-    }
-  }
 
   @Test
   public void testCloneJobFail() throws InterruptedException {
