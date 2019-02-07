0dc94368e3a782585b0359b47c40f2522fdca4d3
Helena Alinder <helenaalinder@gmail.com>
2019-02-07 20:15:23 +0100

    Implement NotifyJob and tests - fix #11
    * Add notify job
    
    * start working with HttpsURLConnection
    
    * remove token
    
    * implement status code on github
    
    * format code, use env variable and add notify event to queue
    
    * format tests
    
    * update comment a bit
    
    * format space

---
+++ src/main/java/se/kth/dd2480/grp25/ci/NotifyJob.java
@@ -0,0 +1,119 @@
+package se.kth.dd2480.grp25.ci;
+
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
+
+/** A job for notifying Github about builds and testing. */
+public class NotifyJob implements Runnable {
+  private Event event;
+  private EventQueue queue;
+  private String token = "";
+
+  public static class Examiner extends JobExaminer {
+
+    public Examiner(EventQueue queue) {
+      super(queue);
+    }
+
+    @Override
+    public Optional<Runnable> offer(Event event) {
+      if (event.getType() == Event.Type.TEST
+          || event.getType() == Event.Type.BUILD
+          || event.getType() == Event.Type.WEB_HOOK) {
+        return Optional.of(new NotifyJob(event, super.queue));
+      } else {
+        return Optional.empty();
+      }
+    }
+  }
+
+  public NotifyJob(Event event, EventQueue queue) {
+    this.event = event;
+    this.queue = queue;
+  }
+
+  @Override
+  public void run() {
+    try {
+      token = System.getenv("TOKEN");
+      if (token == null) {
+        Event notifyEvent =
+            new Event(event.getId(), Event.Type.NOTIFY, Event.Status.FAIL, "Invalid token.");
+        queue.insert(notifyEvent);
+        return;
+      }
+      String buildSuccessful = "\"All tests passed!\"";
+      String buildFail = "\"Build fail\"";
+      String testFail = "\"Some tests failed: " + event.getMessage() + "\"";
+      URL url =
+          new URL(
+              "https://api.github.com/repos/DD2480-Project-group-25/Continuous-Integration/statuses/"
+                  + event.getId()
+                  + "?access_token="
+                  + token);
+      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+      conn.setRequestProperty("Content-Type", "application/json");
+      conn.setDoOutput(true);
+
+      try (OutputStream os = conn.getOutputStream()) {
+        String params = "";
+        Event notifyEvent = null;
+        if (event.getType() == Event.Type.BUILD) {
+          if (event.getStatus() == Event.Status.FAIL) {
+            params = createParams(buildFail);
+            notifyEvent =
+                new Event(
+                    event.getId(),
+                    Event.Type.NOTIFY,
+                    Event.Status.SUCCESSFUL,
+                    "Notified that build failed.");
+          } else {
+            // We don't want to notify the user about a successful build
+            return;
+          }
+        }
+        if (event.getType() == Event.Type.TEST) {
+          if (event.getStatus() == Event.Status.SUCCESSFUL) {
+            params = createParams(buildSuccessful);
+            notifyEvent =
+                new Event(
+                    event.getId(),
+                    Event.Type.NOTIFY,
+                    Event.Status.SUCCESSFUL,
+                    "Notified that build was successful.");
+          } else {
+            params = createParams(testFail);
+            notifyEvent =
+                new Event(
+                    event.getId(),
+                    Event.Type.NOTIFY,
+                    Event.Status.SUCCESSFUL,
+                    "Notified that tests failed.");
+          }
+        }
+        os.write(params.getBytes(StandardCharsets.UTF_8));
+        if (conn.getResponseCode() != HttpURLConnection.HTTP_CREATED) {
+          notifyEvent =
+              new Event(event.getId(), Event.Type.NOTIFY, Event.Status.FAIL, "Failed to notify");
+        }
+        queue.insert(notifyEvent);
+        os.flush();
+      }
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+  }
+
+  private String createParams(String message) {
+    String params =
+        "{\"state\":\"success\",\"target_url\":\"https://api.github.com/repos/DD2480-Project-group-25/Continuous-Integration/build/"
+            + event.getId()
+            + "\",\"description\":"
+            + message
+            + ",\"context\":\"DD2480 CI\"}";
+    return params;
+  }
+}
---
+++ src/test/java/TestNotifyJob.java
@@ -0,0 +1,83 @@
+import org.junit.Assert;
+import org.junit.Test;
+import se.kth.dd2480.grp25.ci.*;
+
+/** */
+public class TestNotifyJob {
+
+  @Test
+  public void testNotifyJobTestPassed() {
+    EventQueue queue = new EventQueue();
+    Event event =
+        new Event(
+            "321c3452974ccfa0d4fb11d7b584f68472fcbabc",
+            Event.Type.TEST,
+            Event.Status.SUCCESSFUL,
+            "Nothing");
+    NotifyJob job = new NotifyJob(event, queue);
+    job.run();
+    try {
+      Event notifyEvent = queue.pop();
+      Assert.assertEquals(Event.Status.SUCCESSFUL, notifyEvent.getStatus());
+      Assert.assertEquals("Notified that build was successful.", notifyEvent.getMessage());
+    } catch (InterruptedException e) {
+      System.out.println(e);
+      Assert.fail();
+    }
+  }
+
+  @Test
+  public void testNotifyJobTestFailed() {
+    EventQueue queue = new EventQueue();
+    Event event =
+        new Event(
+            "fdd1f1e31c6f67d23e1134b34d2cc184f1c5f105",
+            Event.Type.TEST,
+            Event.Status.FAIL,
+            "Failed on LIC0 tests");
+    NotifyJob job = new NotifyJob(event, queue);
+    job.run();
+    try {
+      Event notifyEvent = queue.pop();
+      Assert.assertEquals(Event.Status.SUCCESSFUL, notifyEvent.getStatus());
+      Assert.assertEquals("Notified that tests failed.", notifyEvent.getMessage());
+    } catch (InterruptedException e) {
+      System.out.println(e);
+      Assert.fail();
+    }
+  }
+
+  @Test
+  public void testNotifyJobBuildFailed() {
+    EventQueue queue = new EventQueue();
+    Event event =
+        new Event(
+            "fdd1f1e31c6f67d23e1134b34d2cc184f1c5f105", Event.Type.BUILD, Event.Status.FAIL, "");
+    NotifyJob job = new NotifyJob(event, queue);
+    job.run();
+    try {
+      Event notifyEvent = queue.pop();
+      Assert.assertEquals(Event.Status.SUCCESSFUL, notifyEvent.getStatus());
+      Assert.assertEquals("Notified that build failed.", notifyEvent.getMessage());
+    } catch (InterruptedException e) {
+      System.out.println(e);
+      Assert.fail();
+    }
+  }
+
+  @Test
+  public void testNotifyJobFailed() {
+    EventQueue queue = new EventQueue();
+    Event event = new Event("wrong_commit", Event.Type.TEST, Event.Status.SUCCESSFUL, "");
+    NotifyJob job = new NotifyJob(event, queue);
+    job.run();
+    try {
+      Event notifyEvent = queue.pop();
+      Assert.assertEquals(Event.Status.FAIL, notifyEvent.getStatus());
+      Assert.assertEquals("Failed to notify", notifyEvent.getMessage());
+    } catch (InterruptedException e) {
+      System.out.println(e);
+      Assert.fail();
+    }
+  }
+}
