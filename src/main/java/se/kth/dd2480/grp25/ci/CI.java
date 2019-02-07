2ff14cae87233957cb4b4fdd27c49ca15b0aca14
Marcus Granström <marcus.granstroem@gmail.com>
2019-02-07 23:06:55 +0100

    changes CiServer to take a port argument
    This makes it easier to test since we can use randomized ports

--- src/main/java/se/kth/dd2480/grp25/ci/CI.java
+++ src/main/java/se/kth/dd2480/grp25/ci/CI.java
@@ -2,13 +2,16 @@ package se.kth.dd2480.grp25.ci;
 
 import java.io.IOException;
 
+
 /** Entry class. */
 public class CI {
+
   public static void main(String[] args) {
+    final int port = 8000;
     EventQueue q = new EventQueue();
     CiServer server = null;
     try {
-      server = new CiServer(q);
+      server = new CiServer(q, port);
     } catch (IOException e) {
       System.err.println("Can't start web server!");
       e.printStackTrace();
